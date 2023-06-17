5b392e80ce78961063e191ffb5db5603fcdc1c5d
Hakan <hakantekir2002@outlook.com>
2023-06-17 17:56:48 +0300

    feat: Add mongo connection functions to producer

--- producer.js
+++ producer.js
@@ -1,3 +1,34 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const productSchema = require('./Schema/product');
+dotenv.config();
 
+try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to Mongo Successfully!");
+} catch (error) {
+    console.log(error);
+}
+
+const getLastDocument = async () => {
+    try {
+        const lastDocument = await productSchema.findOne().sort({ _id: -1 });
+        return lastDocument;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getLastDocument().then((lastDocument) => {
+    if (!lastDocument) {
+        const product = new productSchema({
+            value: 0
+        });
+        product.save().then(() => {
+            console.log("Product created successfully!");
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+})
