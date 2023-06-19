ca5fb902469d3ec4a4d47699adf0227a93d6ebfa
Hakan <hakantekir2002@outlook.com>
2023-06-19 13:09:59 +0300

    fix: Fix group id bug

--- Consumer/consumer.js
+++ Consumer/consumer.js
@@ -1,11 +1,12 @@
 const Kafka = require('kafkajs');
+const os = require("os");
 
 const kafka = new Kafka.Kafka ({
     clientId: 'my-app',
     brokers: ['kafka:9092']
 });
 
-const consumer = kafka.consumer({ groupId: 'my-group' });
+const consumer = kafka.consumer({ groupId: os.hostname() });
 consumer.connect();
 consumer.subscribe({ topic: 'product'});
 
